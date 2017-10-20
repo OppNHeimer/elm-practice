d8cbc810b2085734d8f7e4ee25cc63646b8c48e2
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-10-20 09:00:49 -0400

    only failures

---
+++ src/Main.elm
@@ -0,0 +1,83 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "blue" ]
+        [ div [ class "header" ]
+            [ h1 [] [ text "MemorTree" ]
+            , h4 [] [ text "an app to store moments."]
+            ]
+        , img [ src "/logo.svg" ] []
+        , div [] [ text "Your Elm App is working!" ]
+        -- , makeGrid 4
+
+        ]
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+-- makeGridUnit : List ()
+-- makeGridUnit num = 
+--     [num]
+
+
+makeGridUnitList : Int -> a -> List a
+makeGridUnitList num x =
+    if num <= 0 then
+        []
+    else 
+        x :: makeGridUnitList (num-1) x
+
+renderGrid : List (String) -> Html Msg
+renderGrid list =
+    div [ class "grid" ] 
+        (List.map (\l -> div [] [ text l ]) list)
+    
+makeGrid : Int -> Html Msg
+makeGrid num =
+    num 
+    |> makeGridUnitList
+    |> renderGrid
